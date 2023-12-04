77ac0771075e5ee65077c28e83b8051203e37d66
whoooooooooooooooo <quiet.rabbit.03191@gmail.com>
2023-12-04 23:48:26 +0400

    Создан контроллер правонарушений

---
+++ UFSIN/Controllers/ConvictController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UFSIN.Models;
+
+namespace UFSIN.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConvictController : ControllerBase
+    {
+        private static readonly List<Convict> _convicts = new List<Convict>() { };
+        public ConvictController() { }
+        [HttpGet]
+        public IEnumerable<Convict> GetAll()
+        {
+            return _convicts;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Convict bailiff)
+        {
+            _convicts.Add(bailiff);
+            return Created("/bailiffs/", bailiff);
+        }
+    }
+}
